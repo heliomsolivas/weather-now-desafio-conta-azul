3155013489cc2926385b55056dabaa9f547c67e0
heliomsolivas-solucx <helio@solucx.com.br>
2019-12-20 11:33:13 -0300

    Testing sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
+Sentry.init({
+  dsn: 'https://0eb0e39cd89145569a71cdcf250b8ec6@sentry.io/1863281',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
 import App from './App'
 import './assets/css/style.css'
 
